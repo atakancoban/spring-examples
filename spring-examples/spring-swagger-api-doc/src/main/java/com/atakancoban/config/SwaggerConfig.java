3dd16de8af61303f51f5647e10f0fefd9e640a29
atakancoban <atakancobann@gmail.com>
2021-06-10 23:16:22 +0300

    dokuman goruntuleme url

--- spring-examples/spring-swagger-api-doc/src/main/java/com/atakancoban/config/SwaggerConfig.java
+++ spring-examples/spring-swagger-api-doc/src/main/java/com/atakancoban/config/SwaggerConfig.java
@@ -12,6 +12,8 @@ import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+//Dokuman goruntuleme url localhost:8081/swagger-ui.html
+
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
